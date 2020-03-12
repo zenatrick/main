65c22cbf36c121488aaaffd64c48c14354050b49
Chau Teng Le <tenglec@u.nus.edu>
2020-03-13 03:39:32 +0800

    Add Exceptions for operations on TransportBooking

---
+++ src/main/java/seedu/address/model/transportation/exceptions/TransportBookingNotFoundException.java
@@ -0,0 +1,6 @@
+package seedu.address.model.transportation.exceptions;
+
+/**
+ * Signals that the operation is unable to find the specified transport booking.
+ */
+public class TransportBookingNotFoundException extends RuntimeException {}
