21f09fda76ff3d34849f692e90aaf48dbf87e662
jwee97 <jwee97@users.noreply.github.com>
2020-03-17 15:47:27 +0800

    Add AccommodationBookingStorage.java

---
+++ src/main/java/seedu/address/storage/accommodationbooking/AccommodationBookingStorage.java
@@ -0,0 +1,50 @@
+package seedu.address.storage.accommodationbooking;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Optional;
+
+import seedu.address.commons.exceptions.DataConversionException;
+import seedu.address.model.AccommodationBookingManager;
+import seedu.address.model.ReadOnlyAccommodationBookingManager;
+
+/**
+ * Represents a storage for {@link AccommodationBookingManager}.
+ */
+public interface AccommodationBookingStorage {
+
+    /**
+     * Returns the file path of the data file.
+     */
+    Path getAccommodationBookingStorageFilePath();
+
+    /**
+     * Returns AccommodationBookingManager data as a {@link ReadOnlyAccommodationBookingManager}.
+     * Returns {@code Optional.empty()} if storage file is not found.
+     *
+     * @throws DataConversionException if the data in storage is not in the expected format.
+     * @throws IOException             if there was any problem when reading from the storage.
+     */
+    Optional<ReadOnlyAccommodationBookingManager> readTransportBookings() throws DataConversionException, IOException;
+
+    /**
+     * @see #getAccommodationBookingStorageFilePath()
+     */
+    Optional<ReadOnlyAccommodationBookingManager> readAccommodationBookings(Path filePath)
+            throws DataConversionException, IOException;
+
+    /**
+     * Saves the given {@link ReadOnlyAccommodationBookingManager} to the storage.
+     *
+     * @param accommodationBookingManager cannot be null.
+     * @throws IOException if there was any problem writing to the file.
+     */
+    void saveAccommodationBookings(ReadOnlyAccommodationBookingManager accommodationBookingManager) throws IOException;
+
+    /**
+     * @see #saveAccommodationBookings(ReadOnlyAccommodationBookingManager)
+     */
+    void saveTransportBookings(
+            ReadOnlyAccommodationBookingManager accommodationBookingManager, Path filePath) throws IOException;
+
+}
