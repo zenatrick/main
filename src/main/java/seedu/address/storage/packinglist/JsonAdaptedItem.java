f9ba504a918834ac9922d0cac3c6842dc6ded7aa
Catherine <catherineloy99@gmail.com>
2020-03-18 12:33:21 +0800

    Create JsonAdaptedItem.java

---
+++ src/main/java/seedu/address/storage/packinglist/JsonAdaptedItem.java
@@ -0,0 +1,51 @@
+package seedu.address.storage.packinglist;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.packinglistitem.Item;
+import seedu.address.model.packinglistitem.ItemName;
+import seedu.address.model.packinglistitem.Quantity;
+
+public class JsonAdaptedItem {
+    public static final String MISSING_FIELD_MESSAGE_FORMAT = "Item's %s field is missing!";
+
+    private final String name;
+    private final Integer quantity;
+    private final boolean isCheck;
+
+    @JsonCreator
+    public JsonAdaptedItem(@JsonProperty("name") String name,
+                           @JsonProperty("quantity") Integer quantity,
+                           @JsonProperty("isCheck") boolean isCheck) {
+        this.name = name;
+        this.quantity = quantity;
+        this.isCheck = isCheck;
+    }
+
+    public JsonAdaptedItem(Item source) {
+        name = source.getItemName().value;
+        quantity = source.getQuantity().value;
+        isCheck = source.isChecked();
+    }
+
+    public Item toModelType() throws IllegalValueException {
+        if (name == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT),
+                    ItemName.class.getSimpleName()));
+        }
+        if (!ItemName.isValidName(name)) {
+            throw new IllegalValueException(ItemName.MESSAGE_CONSTRAINTS);
+        }
+        final ItemName modelItemName = new ItemName(name);
+
+        if (quantity == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT),
+                    Quantity.class.getSimpleName()));
+        }
+        if (!Quantity.isValidQuantity(quantity)) {
+            throw new IllegalValueException(Quantity.MESSAGE_CONSTRAINTS);
+        }
+        final Quantity modelQuantity = new Quantity(quantity);
+    }
+}
