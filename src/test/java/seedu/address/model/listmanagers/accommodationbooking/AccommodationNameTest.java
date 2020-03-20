e42872b2391ca854f3907d92f0f218f90e53c03a
joshlay96 <x3tvaddict@hotmail.com>
2020-03-20 10:04:51 +0800

    Add Tests for AccommodationBooking

---
+++ src/test/java/seedu/address/model/listmanagers/accommodationbooking/AccommodationNameTest.java
@@ -0,0 +1,82 @@
+package seedu.address.model.listmanagers.accommodationbooking;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+class AccommodationNameTest {
+
+    @Test
+    public void constructorNullThrowsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new AccommodationName(null));
+    }
+
+    @Test
+    public void constructorInvalidAccommodationNameThrowsIllegalArgumentException() {
+        String invalidName = "";
+        assertThrows(IllegalArgumentException.class, () -> new AccommodationName(invalidName));
+    }
+
+    @Test
+    public void isValidName() {
+        // null name
+        assertThrows(NullPointerException.class, () -> AccommodationName.isValidName(null));
+
+        // blank name
+        assertFalse(AccommodationName.isValidName("")); // Empty string
+        assertFalse(AccommodationName.isValidName("   ")); // Multiple Spaces
+        assertFalse(AccommodationName.isValidName(" ")); // Single space
+
+        // invalid name
+        assertFalse(AccommodationName.isValidName("^")); // Non-alphanumeric characters
+        assertFalse(AccommodationName.isValidName("Dog*")); // Contains non-alphanumeric characters.
+        assertFalse(AccommodationName.isValidName("yoursystemrunsf1234anyhowjustpresswhymylifesobadnow"
+                + "IwanttowithdrawfromNUS")); // Contains 74 characters.
+
+
+        // Valid name
+        assertTrue(AccommodationName.isValidName("Jeffry ")); //Normal case
+        assertTrue(AccommodationName.isValidName("Hello1234")); // Contains alphanumeric characters.
+        assertTrue(AccommodationName.isValidName("hellomynameisJ ohn123an dIamA"
+                + "Dog")); //Contains 30 alphanumeric char with spaces.
+        assertTrue(AccommodationName.isValidName("yoursystemrunsf1234anyhow"
+                + "justpress")); // Contains 34 characters.
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals("Joshua", new AccommodationName("Joshua").toString());
+        assertEquals("Kenny", new AccommodationName("Kenny").toString()); //Caps and non caps characters
+        assertEquals("Lum123", new AccommodationName("Lum123").toString()); //Alphanumeric
+
+
+        //Wrong toString
+        assertNotEquals("Big Boss", new AccommodationName("Boss").toString());
+
+    }
+
+    @Test
+    void testEquals() {
+        //Equal Name Object
+        assertEquals(new AccommodationName("Henry"), new AccommodationName("Henry")); // When two names are the same.
+
+        //Non Equal Name Object
+        assertNotEquals(new AccommodationName("Wolfy"), new AccommodationName("Harry")); // When two names are diff
+    }
+
+    @Test
+    public void testHashCode() {
+        // Equal Hashcode
+        assertEquals(new AccommodationName("Hashcode").hashCode(), new AccommodationName("Hashcode").hashCode());
+
+        //Non Equal Hashcode
+        assertNotEquals(new AccommodationName("hashcode").hashCode(), new AccommodationName("HashCode").hashCode());
+
+    }
+
+
+}
---
+++ src/test/java/seedu/address/model/listmanagers/accommodationbooking/DayTest.java
@@ -0,0 +1,64 @@
+package seedu.address.model.listmanagers.accommodationbooking;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+class DayTest {
+
+    @Test
+    public void constructorNullThrowsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Day(null));
+    }
+
+    @Test
+    public void constructorInvalidDayThrowsIllegalArgumentException() {
+        Integer invalidDay = -100;
+        assertThrows(IllegalArgumentException.class, () -> new Day(invalidDay));
+    }
+
+    @Test
+    public void isValidDay() {
+
+        // null Day
+        assertThrows(NullPointerException.class, () -> Day.isValidDay(null));
+
+        // Invalid Day
+        assertFalse(Day.isValidDay(0)); // zero day
+        assertFalse(Day.isValidDay(-100)); // negative day
+
+        //TODO After Wee finds a way to check for overlap
+        //assertFalse(Day.isAmount.isValidAmount("1000.000"));
+
+        //Valid Day
+        assertTrue(Day.isValidDay(10)); // With 2 digits.
+        assertTrue(Day.isValidDay(1)); // normal test
+    }
+
+
+    @Test
+    void testToString() {
+        assertEquals("10", new Day(10).toString());
+    }
+
+    @Test
+    void testEquals() {
+        assertEquals(new Day(10), new Day(10)); // WHen 2 days are the same
+        assertNotEquals(new Day(15), new Day(5)); // WHen 2 days are different.
+    }
+
+    @Test
+    void testHashCode() {
+        // Equal Hashcode
+        assertEquals(new Day(1).hashCode(), new Day(1).hashCode());
+
+        //Non Equal Hashcode
+        assertNotEquals(new Day(1).hashCode(), new Day(2).hashCode());
+
+
+    }
+}
