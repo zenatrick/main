d66e3588303bc5b93f963c2f626b6759032fbd75
jwee97 <jwee97@users.noreply.github.com>
2020-03-20 17:14:01 +0800

    Add AddAccommodationBookingCommand

---
+++ src/main/java/seedu/address/logic/commands/accommodationbooking/AddAccommodationBookingCommand.java
@@ -0,0 +1,70 @@
+package seedu.address.logic.commands.accommodationbooking;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ACCOMMODATION_END_DAY;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ACCOMMODATION_LOCATION;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ACCOMMODATION_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ACCOMMODATION_REMARK;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ACCOMMODATION_START_DAY;
+
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.listmanagers.accommodationbooking.AccommodationBooking;
+
+/**
+ * Adds an AccommodationBooking to the AccommodationBookingManager.
+ */
+public class AddAccommodationBookingCommand extends Command {
+
+    public static final String COMMAND_WORD = "addacc";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds an accommodation booking. "
+            + "Parameters: "
+            + PREFIX_ACCOMMODATION_NAME + "NAME "
+            + PREFIX_ACCOMMODATION_LOCATION + "LOCATION "
+            + PREFIX_ACCOMMODATION_START_DAY + "START_DAY "
+            + PREFIX_ACCOMMODATION_END_DAY + "ADDRESS "
+            + "[" + PREFIX_ACCOMMODATION_REMARK + "REMARK]\n"
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_ACCOMMODATION_NAME + "JW Marriott Hotel "
+            + PREFIX_ACCOMMODATION_LOCATION + "KL "
+            + PREFIX_ACCOMMODATION_START_DAY + "2 "
+            + PREFIX_ACCOMMODATION_END_DAY + "4 "
+            + PREFIX_ACCOMMODATION_REMARK + "Check-in at 3pm.\n";
+
+    public static final String MESSAGE_SUCCESS = "New accommodation booking added: %1$s";
+    public static final String MESSAGE_DUPLICATE_ACCOMMODATION_BOOKING = "This accommodation booking already exists "
+            + "in the list";
+
+    private final AccommodationBooking toAdd;
+
+    /**
+     * Creates an AddCommand to add the specified {@code Person}
+     */
+    public AddAccommodationBookingCommand(AccommodationBooking accommodationBooking) {
+        requireNonNull(accommodationBooking);
+        toAdd = accommodationBooking;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+
+        if (model.hasAccommodationBooking(toAdd)) {
+            throw new CommandException(MESSAGE_DUPLICATE_ACCOMMODATION_BOOKING);
+        }
+
+        model.addAccommodationBooking(toAdd);
+        return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof AddAccommodationBookingCommand // instanceof handles nulls
+                && toAdd.equals(((AddAccommodationBookingCommand) other).toAdd));
+    }
+
+}
