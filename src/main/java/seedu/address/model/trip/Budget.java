8f3ff7fe05cf0034ba3d35d45d7b6a290c9b8ffb
Deunitato <charlotte.limwt@gmail.com>
2020-03-22 11:05:02 +0800

    Change Budget type to integer

--- src/main/java/seedu/address/model/trip/Budget.java
+++ src/main/java/seedu/address/model/trip/Budget.java
@@ -1,5 +1,6 @@
 package seedu.address.model.trip;
 
+import static seedu.address.commons.util.AppUtil.checkArgument;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
 /**
@@ -8,15 +9,25 @@ import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
  */
 public class Budget {
 
-    private final String amount;
+    private final Integer amount;
+    public static final String MESSAGE_CONSTRAINTS =
+            "Budget should be a whole number greater than 0";
 
-    public Budget(String amount) {
+    public Budget(Integer amount) {
         requireAllNonNull(amount);
+        checkArgument(isValidBudget(amount), MESSAGE_CONSTRAINTS);
         this.amount = amount;
     }
 
-    public String getBudget() {
+    public Integer getBudget() {
         return amount;
+    }
+
+    /**
+     * Returns true if a given Integer is a valid budget number.
+     */
+    public static boolean isValidBudget(Integer test) {
+        return test > 0;
     }
 
     @Override
