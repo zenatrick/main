785501dd7e8dbbd2813b93972c76dfbb597cdde1
Catherine <catherineloy99@gmail.com>
2020-03-26 18:56:53 +0800

    Update FindItemCommand.java

--- src/main/java/seedu/address/logic/commands/packinglist/FindItemCommand.java
+++ src/main/java/seedu/address/logic/commands/packinglist/FindItemCommand.java
@@ -3,37 +3,51 @@ package seedu.address.logic.commands.packinglist;
 import static java.util.Objects.requireNonNull;
 
 import seedu.address.commons.core.Messages;
+import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
-import seedu.address.logic.commands.FindCommand;
 import seedu.address.model.Model;
-import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.model.listmanagers.packinglistitem.ItemContainsKeywordsPredicate;
 
-public class FindItemCommand {
-    public static final String COMMAND_WORD = "find";
+/**
+ * The type Find item command.
+ */
+public class FindItemCommand extends Command {
+    /**
+     * The constant COMMAND_WORD.
+     */
+    public static final String COMMAND_WORD = "finditem";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose names contain any of "
+    /**
+     * The constant MESSAGE_USAGE.
+     */
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all items whose names contain any of "
             + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
-            + "Example: " + COMMAND_WORD + " alice bob charlie";
+            + "Example: " + COMMAND_WORD + " shirt car shampoo";
 
-    private final NameContainsKeywordsPredicate predicate;
+    private final ItemContainsKeywordsPredicate predicate;
 
-    public FindCommand(NameContainsKeywordsPredicate predicate) {
+    /**
+     * Instantiates a new Find item command.
+     *
+     * @param predicate the predicate
+     */
+    public FindItemCommand(ItemContainsKeywordsPredicate predicate) {
         this.predicate = predicate;
     }
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        model.updateFilteredPersonList(predicate);
+        model.updateFilteredPackingList(predicate);
         return new CommandResult(
-                String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
+                String.format(Messages.MESSAGE_ITEMS_LISTED_OVERVIEW, model.getFilteredPackingList().size()));
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
-                || (other instanceof FindCommand // instanceof handles nulls
-                && predicate.equals(((FindCommand) other).predicate)); // state check
+                || (other instanceof FindItemCommand // instanceof handles nulls
+                && predicate.equals(((FindItemCommand) other).predicate)); // state check
     }
 }
