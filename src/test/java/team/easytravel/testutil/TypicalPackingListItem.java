3b5498f209a24aa828905f6b0dc1283c79db99b6
Deunitato <charlotte.limwt@gmail.com>
2020-04-10 12:49:18 +0800

    Update TypicalPackingListItem.java

--- src/test/java/team/easytravel/testutil/TypicalPackingListItem.java
+++ src/test/java/team/easytravel/testutil/TypicalPackingListItem.java
@@ -16,13 +16,22 @@ import team.easytravel.model.listmanagers.packinglistitem.Quantity;
 public class TypicalPackingListItem {
 
     public static final PackingListItem PACKING_LIST_SHIRT = new PackingListItem(
-            new ItemName("Tshirt"), new Quantity(7), new ItemCategory("basics"), false);
+            new ItemName("Tshirt"), new Quantity(7), new ItemCategory("clothes"), false);
 
     public static final PackingListItem PACKING_LIST_JEANS = new PackingListItem(
-            new ItemName("Jeans"), new Quantity(5), new ItemCategory("basics"), false);
+            new ItemName("Jeans"), new Quantity(5), new ItemCategory("clothes"), false);
 
     public static final PackingListItem PACKING_LIST_UNDERWEAR = new PackingListItem(
-            new ItemName("Underwear"), new Quantity(7), new ItemCategory("essentials"), false);
+            new ItemName("Underwear"), new Quantity(7), new ItemCategory("clothes"), false);
+
+    public static final PackingListItem PACKING_LIST_PASSPORT = new PackingListItem(
+            new ItemName("Passport"), new Quantity(1), new ItemCategory("international"), false);
+
+    public static final PackingListItem PACKING_LIST_SHAMPOO = new PackingListItem(
+            new ItemName("Shampoo"), new Quantity(1), new ItemCategory("toiletries"), false);
+
+    public static final PackingListItem PACKING_LIST_CONDITIONER = new PackingListItem(
+            new ItemName("Conditioner"), new Quantity(1), new ItemCategory("toiletries"), false);
 
     private TypicalPackingListItem() {
     }
@@ -41,7 +50,7 @@ public class TypicalPackingListItem {
 
     public static List<PackingListItem> getTypicalPackingListItems() {
         return new ArrayList<>(Arrays.asList(PACKING_LIST_UNDERWEAR, PACKING_LIST_SHIRT,
-                PACKING_LIST_JEANS));
+                PACKING_LIST_JEANS, PACKING_LIST_CONDITIONER, PACKING_LIST_PASSPORT, PACKING_LIST_SHAMPOO));
     }
 
 
