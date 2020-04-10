7ce39f8907c00aed2e6eb077a77610e6a98bbe17
Deunitato <charlotte.limwt@gmail.com>
2020-04-10 14:50:41 +0800

    Add TripTest

---
+++ src/test/java/team/easytravel/model/trip/TripTest.java
@@ -0,0 +1,142 @@
+package team.easytravel.model.trip;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import team.easytravel.commons.core.time.Date;
+import team.easytravel.model.util.attributes.Title;
+import team.easytravel.testutil.Assert;
+
+class TripTest {
+
+    private final Trip trip = new Trip(new Title("Cheese Land"),
+            Date.fromString("28-09-2020"), Date.fromString("05-10-2020"),
+            new Budget(1000), new ExchangeRate(1.03));
+
+
+    @Test
+    public void constructorInvalidTitleThrowsIllegalArgumentException() {
+        String invalidTitle = " ";
+
+        Assert.assertThrows(IllegalArgumentException.class, () -> new Trip(new Title(invalidTitle),
+                Date.fromString("28-09-2020"), Date.fromString("05-10-2020"),
+                new Budget(1000), new ExchangeRate(1.03)));
+    }
+
+    @Test
+    public void constructorInvalidStartThrowsIllegalArgumentException() {
+        String invalidStart = " ";
+
+        Assert.assertThrows(IllegalArgumentException.class, () -> new Trip(new Title("Cheese Land"),
+                Date.fromString(invalidStart), Date.fromString("05-10-2020"),
+                new Budget(1000), new ExchangeRate(1.03)));
+    }
+
+    @Test
+    public void constructorInvalidEndThrowsIllegalArgumentException() {
+        String invalidEnd = " ";
+
+        Assert.assertThrows(IllegalArgumentException.class, () -> new Trip(new Title("Cheese Land"),
+                Date.fromString("28-09-2020"), Date.fromString(invalidEnd),
+                new Budget(1000), new ExchangeRate(1.03)));
+    }
+
+    @Test
+    public void constructorInvalidBudgetThrowsIllegalArgumentException() {
+        Integer invalidBudget = -222; //negative
+
+        Assert.assertThrows(IllegalArgumentException.class, () -> new Trip(new Title("Cheese Land"),
+                Date.fromString("28-09-2020"), Date.fromString("10-05-2020"),
+                new Budget(invalidBudget), new ExchangeRate(1.03)));
+    }
+    @Test
+    public void constructorInvalidExchangeRateThrowsIllegalArgumentException() {
+        Double invalidExchangeRate = -0.3; //negative
+
+        Assert.assertThrows(IllegalArgumentException.class, () -> new Trip(new Title("Cheese Land"),
+                Date.fromString("28-09-2020"), Date.fromString("10-05-2020"),
+                new Budget(1000), new ExchangeRate(invalidExchangeRate)));
+    }
+
+
+    @Test
+    public void getTitle() {
+
+        // Correct case
+        Assertions.assertEquals(new Title("Cheese Land"), trip.getTitle());
+
+        //Different case
+        Assertions.assertNotEquals(new Title("Not Cheese Land"), trip.getTitle());
+
+    }
+
+    @Test
+    public void getDate() {
+
+        // Correct case
+        assertEquals(Date.fromString("28-09-2020"),
+                trip.getStartDate());
+
+        //Different case
+        assertNotEquals(Date.fromString("20-09-2020"),
+                trip.getStartDate());
+
+        // Correct case
+        assertEquals(Date.fromString("05-10-2020"),
+                trip.getEndDate());
+
+        //Different case
+        assertNotEquals(Date.fromString("20-09-2020"),
+                trip.getEndDate());
+
+
+
+    }
+
+    @Test
+    public void getBudget() {
+
+        // Correct case
+        assertEquals(new Budget(1000).toString(),
+                trip.getBudget().toString());
+
+        //Different case
+        assertNotEquals(new Budget(2000).toString(),
+                trip.getBudget().toString());
+
+    }
+
+    @Test
+    public void getExchangeRate() {
+
+        // Correct case
+        assertEquals(new ExchangeRate(1.03).toString(),
+                trip.getExchangeRate().toString());
+
+        //Different case
+        assertNotEquals(new ExchangeRate(1.3).toString(),
+                trip.getExchangeRate().toString());
+
+    }
+
+
+    @Test
+    public void testToString() {
+        Trip editedTrip =
+                new Trip(new Title("Cheese Land"),
+                        Date.fromString("28-09-2020"), Date.fromString("05-10-2020"),
+                        new Budget(1000), new ExchangeRate(1.03));
+
+        assertEquals(trip.toString(), editedTrip.toString());
+
+        editedTrip = new Trip(new Title("Not Cheese Land"),
+                        Date.fromString("28-09-2020"), Date.fromString("05-10-2020"),
+                        new Budget(1000), new ExchangeRate(1.03));
+
+        assertNotEquals(editedTrip.toString(), trip.toString()); //different title
+
+    }
+}
