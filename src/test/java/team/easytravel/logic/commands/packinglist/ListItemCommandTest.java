00825ecbdc50242c4d404ae11ec1ac37beb80f48
Catherine <catherineloy99@gmail.com>
2020-04-11 13:03:52 +0800

    Create ListItemCommandTest.java

---
+++ src/test/java/team/easytravel/logic/commands/packinglist/ListItemCommandTest.java
@@ -0,0 +1,67 @@
+package team.easytravel.logic.commands.packinglist;
+
+import static team.easytravel.logic.commands.CommandTestUtil.assertPackingListItemCommandSuccess;
+import static team.easytravel.logic.commands.CommandTestUtil.showPackingListItemAtIndex;
+import static team.easytravel.testutil.TypicalIndexes.INDEX_FIRST;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import team.easytravel.model.Model;
+import team.easytravel.model.ModelManager;
+import team.easytravel.model.listmanagers.AccommodationBookingManager;
+import team.easytravel.model.listmanagers.ActivityManager;
+import team.easytravel.model.listmanagers.FixedExpenseManager;
+import team.easytravel.model.listmanagers.TransportBookingManager;
+import team.easytravel.model.listmanagers.UserPrefs;
+import team.easytravel.model.trip.Trip;
+import team.easytravel.model.trip.TripManager;
+import team.easytravel.testutil.TypicalPackingListItem;
+import team.easytravel.testutil.trip.TripBuilder;
+
+/**
+ * The type List item command test.
+ */
+public class ListItemCommandTest {
+    private Model model;
+    private TripManager tripManagerSet;
+    private Model expectedModel;
+
+    /**
+     * Sets up.
+     */
+    @BeforeEach
+    public void setUp() {
+        Trip newTrip = new TripBuilder().build();
+        tripManagerSet = new TripManager();
+        tripManagerSet.setTrip(newTrip);
+        model = new ModelManager(new TransportBookingManager(),
+                new FixedExpenseManager(), TypicalPackingListItem.getTypicalPackingListManager(), new ActivityManager(),
+                new AccommodationBookingManager(), tripManagerSet,
+                new UserPrefs());
+
+        expectedModel = new ModelManager(new TransportBookingManager(),
+                new FixedExpenseManager(), TypicalPackingListItem.getTypicalPackingListManager(), new ActivityManager(),
+                new AccommodationBookingManager(), tripManagerSet,
+                new UserPrefs());
+    }
+
+    /**
+     * Execute list is not filtered shows same list.
+     */
+    @Test
+    public void execute_listIsNotFiltered_showsSameList() {
+        assertPackingListItemCommandSuccess(new ListItemCommand(),
+                model, ListItemCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    /**
+     * Execute list is filtered shows everything.
+     */
+    @Test
+    public void execute_listIsFiltered_showsEverything() {
+        showPackingListItemAtIndex(model, INDEX_FIRST);
+        assertPackingListItemCommandSuccess(new ListItemCommand(),
+                model, ListItemCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+}
